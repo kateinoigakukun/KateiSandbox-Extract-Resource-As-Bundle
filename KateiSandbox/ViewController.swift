9c3c47be0b29f7e67840173fb562ab058cb522f8
Yuta Saito <kateinoigakukun@gmail.com>
2019-08-14 21:03:55 +0900

    Add sample resource

--- KateiSandbox/ViewController.swift
+++ KateiSandbox/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let bundlePath = Bundle.main.path(forResource: "StaticBundle", ofType: "bundle")!
+        let bundle = Bundle(path: bundlePath)!
+        let helloPath = bundle.path(forResource: "Hello", ofType: "txt")!
+        print(try! String(contentsOfFile: helloPath))
     }
 
 
