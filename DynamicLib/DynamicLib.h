01f8ded56ab871ec2b36a54c570308b471d39faf
Yuta Saito <kateinoigakukun@gmail.com>
2019-08-14 20:23:49 +0900

    Create Dynamic Library

---
+++ DynamicLib/DynamicLib.h
@@ -0,0 +1,19 @@
+//
+//  DynamicLib.h
+//  DynamicLib
+//
+//  Created by Yuta Saito on 2019/08/14.
+//  Copyright © 2019 Yuta Saito. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DynamicLib.
+FOUNDATION_EXPORT double DynamicLibVersionNumber;
+
+//! Project version string for DynamicLib.
+FOUNDATION_EXPORT const unsigned char DynamicLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DynamicLib/PublicHeader.h>
+
+
