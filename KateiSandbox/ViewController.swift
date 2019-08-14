29b6f32186dca75f3a9cecabdbe0678cd065fc54
Yuta Saito <kateinoigakukun@gmail.com>
2019-08-14 20:22:55 +0900

    Initial commit

---
+++ KateiSandbox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KateiSandbox
+//
+//  Created by Yuta Saito on 2019/08/14.
+//  Copyright © 2019 Yuta Saito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
